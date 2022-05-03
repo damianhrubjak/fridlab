980707e22a94e0c5a32081069385ba7bd8c479c0
Damián Hrubják <49727983+damianhrubjak@users.noreply.github.com>
2022-05-03 20:45:34 +0200

    Updated color pallette -> using css variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,10 +14,11 @@ module.exports = {
                 heading: ["'Press Start 2P'", ...defaultTheme.fontFamily.mono],
             },
             colors: {
-                body: "#080212",
-                "fri-yellow": "#feb81c",
-                "prusa-orange": "#f94d0c",
-                "purple-dark": "#560090",
+                body: "var(--color-body)",
+                blue: "var(--color-blue)",
+                "dark-blue": "var(--color-dark-blue)",
+                yellow: "var(--color-yellow)",
+                green: "var(--color-green)",
             },
         },
         screens: {
