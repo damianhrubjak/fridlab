ad78f987ea042c97a350772e2a861ef87491192b
Damián Hrubják <49727983+damianhrubjak@users.noreply.github.com>
2022-05-03 21:36:19 +0200

    Home page sketch v2

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,10 +1,10 @@
-<header class="w-full fixed top-0 left-0 bg-body z-20 py-4">
+<header class="bg-pallette-black shadow-pallette-blue fixed top-0 left-0 z-20 w-full py-4 shadow-md">
     <div class="page-wrapper">
-        <div class="flex justify-between items-center">
-            <div class="logo w-[20%]">
+        <div class="flex items-center justify-between">
+            <div class="logo w-40">
                 <img src="{{ asset('images/Logo/FriDLab-logo-transparent.svg') }}" class="h-full w-[200px]" alt="LOGO">
             </div>
-            <div class="flex gap-4">
+            <div class="flex gap-16">
 
                 <a class="header-link block" href="#">Domov</a>
                 <a class="header-link block" href="#">O nás</a>
@@ -17,11 +17,11 @@
 </header>
 
 <!--
-         <a href="#"><i class="fa-brands fa-tiktok"></i></a>
-                <a href="#"><i class="fa-brands fa-instagram"></i></a>
-                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
-                <a href="#"><i class="fa-brands fa-twitter"></i></a>
-                <a href="#"><i class="fa-brands fa-youtube"></i></a>
-                <a href="#"><i class="fa-brands fa-discord"></i></a>
-                <a href="#"><i class="fa-solid fa-envelope"></i></a>
+<a href="#"><i class="fa-brands fa-tiktok"></i></a>
+<a href="#"><i class="fa-brands fa-instagram"></i></a>
+<a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+<a href="#"><i class="fa-brands fa-twitter"></i></a>
+<a href="#"><i class="fa-brands fa-youtube"></i></a>
+<a href="#"><i class="fa-brands fa-discord"></i></a>
+<a href="#"><i class="fa-solid fa-envelope"></i></a>
 -->
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,30 +4,34 @@
 
 @section('page-content')
 
+    <section class="h-screen pt-28 pb-0">
+        <img class="side-shape left-0" src="{{ asset('images/homepage-illustration-2.svg') }}" alt="">
 
-    <section class="page-section h-screen w-full">
         <div class="page-wrapper flex h-full items-center justify-center">
 
             <div class="h-full w-1/2"></div>
             <div class="relative flex h-full w-1/2 items-center">
                 <div class="z-10 w-full">
-                    <h1 class="font-heading text-8xl leading-snug">Hello, we are good</h1>
+                    <h1 class="font-heading heading-shadow w-3/4 text-7xl leading-snug">Hello, we are good</h1>
 
-                    <p class="mt-16 w-3/4 text-2xl font-medium">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio ea praesentium laudantium temporibus,
+                    <p class="mt-16 w-3/4 text-xl font-medium">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        <br><br>
+                        Optio ea praesentium laudantium temporibus,
                         at adipisci porro iure dolore! Qui eos velit ipsa voluptatibus dolor aperiam quod eius et quas
                         ipsum.
                     </p>
 
                     <div class="mt-16">
-                        <button class="font-heading text-body bg-cyan-300 py-4 px-8 text-xl">Naše
+                        <button class="font-heading bg-pallette-yellow text-pallette-black py-4 px-8 text-xl">Naše
                             projekty</button>
                     </div>
                 </div>
 
             </div>
         </div>
-        <img class="absolute right-0 top-0 h-full opacity-50" src="{{ asset('images/homepage-illustration.svg') }}" alt="">
+        <img class="side-shape right-0 rotate-180" src="{{ asset('images/homepage-illustration-2.svg') }}" alt="">
+
     </section>
 
 @endsection
