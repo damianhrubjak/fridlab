a8619d5d817f1b1512a0ad7103759a593305cd83
borisbrnkalak <borisbrnkalak@gmail.com>
2022-05-04 11:10:22 +0200

    Added colors into social icons

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -16,13 +16,13 @@
 
         <div
             class="absolute bottom-0 right-0 -mb-[28px] bg-pallette-black px-3 shadow-pallette-blue py-1 rounded z-10 social-links-wrapper">
-            <a href="#"><i class="header-social-links fa-brands fa-tiktok"></i></a>
-            <a href="#"><i class="header-social-links fa-brands fa-instagram"></i></a>
-            <a href="#"><i class="header-social-links fa-brands fa-facebook-f"></i></a>
-            <a href="#"><i class="header-social-links fa-brands fa-twitter"></i></a>
-            <a href="#"><i class="header-social-links fa-brands fa-youtube"></i></a>
-            <a href="#"><i class="header-social-links fa-brands fa-discord"></i></a>
-            <a href="#"><i class="header-social-links fa-solid fa-envelope"></i></a>
+            <a href="#"><i class="header-social-links fa-brands fa-tiktok hover:text-[#EE1D52]"></i></a>
+            <a href="#"><i class="header-social-links fa-brands fa-instagram instagram"></i></a>
+            <a href="#"><i class="header-social-links fa-brands fa-facebook-f hover:text-[#4267B2]"></i></a>
+            <a href="#"><i class="header-social-links fa-brands fa-twitter hover:text-[#1DA1F2]"></i></a>
+            <a href="#"><i class="header-social-links fa-brands fa-youtube hover:text-[#FF0000]"></i></a>
+            <a href="#"><i class="header-social-links fa-brands fa-discord hover:text-[#7289DA]"></i></a>
+            <a href="#"><i class="header-social-links fa-solid fa-envelope hover:text-[#FBBC05]"></i></a>
         </div>
 
     </div>
