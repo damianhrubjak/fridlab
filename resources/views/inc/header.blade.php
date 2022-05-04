101269c7fdb15e804eb75a77901189d435e3edb2
borisbrnkalak <borisbrnkalak@gmail.com>
2022-05-04 10:43:54 +0200

    Added social links to header

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,19 +1,34 @@
-<header class="bg-pallette-black shadow-pallette-blue fixed top-0 left-0 z-20 w-full py-4 shadow-md">
-    <div class="page-wrapper">
+<header class="bg-pallette-black shadow-pallette-blue fixed top-0 left-0 z-20 w-full shadow-md">
+    <div class="page-wrapper py-4 relative">
         <div class="flex items-center justify-between">
             <div class="logo w-40">
                 <img src="{{ asset('images/Logo/FriDLab-logo-transparent.svg') }}" class="h-full w-[200px]" alt="LOGO">
             </div>
             <div class="flex gap-16">
 
-                <a class="header-link block" href="#">Domov</a>
+                <a class="header-link block active" href="#">Domov</a>
                 <a class="header-link block" href="#">O nás</a>
                 <a class="header-link block" href="#">Kontakt</a>
                 <a class="header-link block" href="#">Projekty</a>
 
             </div>
         </div>
+
+        <div
+            class="absolute bottom-0 right-0 -mb-[28px] bg-pallette-black px-3 shadow-pallette-blue py-1 rounded z-10 social-links-wrapper">
+            <a href="#"><i class="header-social-links fa-brands fa-tiktok"></i></a>
+            <a href="#"><i class="header-social-links fa-brands fa-instagram"></i></a>
+            <a href="#"><i class="header-social-links fa-brands fa-facebook-f"></i></a>
+            <a href="#"><i class="header-social-links fa-brands fa-twitter"></i></a>
+            <a href="#"><i class="header-social-links fa-brands fa-youtube"></i></a>
+            <a href="#"><i class="header-social-links fa-brands fa-discord"></i></a>
+            <a href="#"><i class="header-social-links fa-solid fa-envelope"></i></a>
+        </div>
+
     </div>
+
+
+
 </header>
 
 <!--
