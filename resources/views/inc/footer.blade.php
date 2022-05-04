ed0673aad2d0fd6670215e621a1dab7543647f58
Damián Hrubják <49727983+damianhrubjak@users.noreply.github.com>
2022-05-04 11:17:50 +0200

    Created footer

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,12 +1,88 @@
-<footer>
-
-    <div class="w-full">
-        <div class="page-wrapper"></div>
+<footer class="bg-pallette-black border-pallette-blue border-t-2">
+    <div class="w-full py-10">
+        <div class="page-wrapper">
+            <div class="grid grid-cols-4 gap-16">
+                <div class="item">
+                    <img src="{{ asset('images/Logo/FriDLab-logo-transparent.svg') }}" alt="" class="w-1/2">
+                    <p class="mt-8 w-5/6">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur officia reiciendis quo
+                        aut tempore numquam impedit eligendi inventore
+                    </p>
+                </div>
+                <div class="item">
+                    <h2 class="font-heading mb-8">Social</h2>
+                    <div class="mt-8 grid grid-cols-2 gap-2">
+                        <div class=""> <a href="" class="mt-2 flex items-center">
+                                <p class="footer-social-icon"><i class="fa-brands fa-tiktok"></i></p>
+                                <p class="">TikTok</p>
+                            </a> <a href="" class="mt-2 flex items-center">
+                                <p class="footer-social-icon"><i class="fa-brands fa-instagram"></i></p>
+                                <p class="">Instagram</p>
+                            </a> <a href="" class="mt-2 flex items-center">
+                                <p class="footer-social-icon"><i class="fa-brands fa-facebook-f"></i></p>
+                                <p class="">Facebook</p>
+                            </a> <a href="" class="mt-2 flex items-center">
+                                <p class="footer-social-icon"><i class="fa-brands fa-twitter"></i></p>
+                                <p class="">Twitter</p>
+                            </a> </div>
+                        <div class="">
+                            <a href="" class="mt-2 flex items-center">
+                                <p class="footer-social-icon"><i class="fa-brands fa-youtube"></i></p>
+                                <p class="">YouTube</p>
+                            </a> <a href="" class="mt-2 flex items-center">
+                                <p class="footer-social-icon"><i class="fa-brands fa-discord"></i></p>
+                                <p class="">Discord</p>
+                            </a> <a href="" class="mt-2 flex items-center">
+                                <p class="footer-social-icon"><i class="fa-solid fa-envelope"></i></p>
+                                <p class="">Mail</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <h2 class="font-heading mb-8">Address</h2>
+                    <div class="mt-8 text-lg">
+                        <p>Somewhere in Zilina 8512</p>
+                        <p>01245, nowhere</p>
+                        <p>Slovakia</p>
+                        <p class="mt-4 font-bold">FRI - Fakulta riadenia a informatiky</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <h2 class="font-heading mb-8">Contact</h2>
+                    <div class="mt-8">
+                        <a href="" class="item-center mt-2 flex">
+                            <div class="footer-social-icon">
+                                <i class="fa-solid fa-phone"></i>
+                            </div>
+                            <p>+421 908 456 123</p>
+                        </a>
+                        <a href="" class="item-center mt-2 flex">
+                            <div class="footer-social-icon">
+                                <i class="fa-solid fa-phone"></i>
+                            </div>
+                            <p>+421 908 456 123</p>
+                        </a>
+                        <a href="" class="item-center mt-2 flex">
+                            <div class="footer-social-icon">
+                                <i class="fa-solid fa-at"></i>
+                            </div>
+                            <p>mail@mail.com</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
-    <div class="w-full">
-        <div class="page-wrapper">
-            &copy;
+    <div class="bg-pallette-yellow text-pallette-black w-full py-4">
+        <div class="page-wrapper flex items-center justify-between">
+            <p class="font-heading text-xs">
+                &copy; 2022 All rights reserved.
+            </p>
+            <p class="font-heading text-xs">
+                Created by 3D tlačiarnicks enthusiasts
+            </p>
         </div>
     </div>
 </footer>
