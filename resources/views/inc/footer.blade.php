be55e6cf15540a029651f78d4116cc9bfde9f750
Damián Hrubják <49727983+damianhrubjak@users.noreply.github.com>
2022-05-04 09:20:19 +0200

    Added footer

---
+++ resources/views/inc/footer.blade.php
@@ -0,0 +1,12 @@
+<footer>
+
+    <div class="w-full">
+        <div class="page-wrapper"></div>
+    </div>
+
+    <div class="w-full">
+        <div class="page-wrapper">
+            &copy;
+        </div>
+    </div>
+</footer>
