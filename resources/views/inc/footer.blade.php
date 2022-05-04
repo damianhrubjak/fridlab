251c9273beb4de06c4ac2f42f26b89783bd5e09e
borisbrnkalak <borisbrnkalak@gmail.com>
2022-05-04 14:40:24 +0200

    Links are now working

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -12,24 +12,28 @@
                 <div class="item">
                     <h2 class="font-heading mb-8">Social</h2>
                     <div class="mt-8 grid grid-cols-2 gap-2">
-                        <div class=""> <a href="" class="footer-social-link hover:text-[#EE1D52]">
+                        <div class=""> <a href="https://www.tiktok.com/@fridlab"
+                                class="footer-social-link hover:text-[#EE1D52]">
                                 <p class="footer-social-icon"><i class="fa-brands fa-tiktok"></i></p>
                                 <p class="">TikTok</p>
-                            </a> <a href="" class="instagram footer-social-link">
+                            </a> <a href="https://www.instagram.com/fridlabuniza/" class="instagram footer-social-link">
                                 <p class="footer-social-icon"><i class="fa-brands fa-instagram"></i></p>
                                 <p class="">Instagram</p>
-                            </a> <a href="" class="footer-social-link hover:text-[#4267B2]">
+                            </a> <a href="https://www.facebook.com/Fridlabuniza"
+                                class="footer-social-link hover:text-[#4267B2]">
                                 <p class="footer-social-icon"><i class="fa-brands fa-facebook-f"></i></p>
                                 <p class="">Facebook</p>
-                            </a> <a href="" class="footer-social-link hover:text-[#1DA1F2]">
+                            </a> <a href="https://twitter.com/FridLab" class="footer-social-link hover:text-[#1DA1F2]">
                                 <p class="footer-social-icon"><i class="fa-brands fa-twitter"></i></p>
                                 <p class="">Twitter</p>
                             </a> </div>
                         <div class="">
-                            <a href="" class="footer-social-link hover:text-[#FF0000]">
+                            <a href="https://www.youtube.com/channel/UCAL9BL74HS6Cl3CVX9K18Ng"
+                                class="footer-social-link hover:text-[#FF0000]">
                                 <p class="footer-social-icon"><i class="fa-brands fa-youtube"></i></p>
                                 <p class="">YouTube</p>
-                            </a> <a href="" class="footer-social-link hover:text-[#7289DA]">
+                            </a> <a href="https://discord.com/invite/vfy4KDaMXY"
+                                class="footer-social-link hover:text-[#7289DA]">
                                 <p class="footer-social-icon"><i class="fa-brands fa-discord"></i></p>
                                 <p class="">Discord</p>
                             </a> <a href="" class="footer-social-link hover:text-[#FBBC05]">
--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -16,12 +16,18 @@
 
         <div
             class="bg-pallette-black shadow-pallette-blue social-links-wrapper absolute bottom-0 right-0 z-10 -mb-[28px] rounded px-3 py-1">
-            <a href="#"><i class="header-social-links fa-brands fa-tiktok hover:text-[#EE1D52]"></i></a>
-            <a href="#"><i class="header-social-links fa-brands fa-instagram instagram"></i></a>
-            <a href="#"><i class="header-social-links fa-brands fa-facebook-f hover:text-[#4267B2]"></i></a>
-            <a href="#"><i class="header-social-links fa-brands fa-twitter hover:text-[#1DA1F2]"></i></a>
-            <a href="#"><i class="header-social-links fa-brands fa-youtube hover:text-[#FF0000]"></i></a>
-            <a href="#"><i class="header-social-links fa-brands fa-discord hover:text-[#7289DA]"></i></a>
+            <a href="https://www.tiktok.com/@fridlab"><i
+                    class="header-social-links fa-brands fa-tiktok hover:text-[#EE1D52]"></i></a>
+            <a href="https://www.instagram.com/fridlabuniza/"><i
+                    class="header-social-links fa-brands fa-instagram instagram"></i></a>
+            <a href="https://www.facebook.com/Fridlabuniza"><i
+                    class="header-social-links fa-brands fa-facebook-f hover:text-[#4267B2]"></i></a>
+            <a href="https://twitter.com/FridLab"><i
+                    class="header-social-links fa-brands fa-twitter hover:text-[#1DA1F2]"></i></a>
+            <a href="https://www.youtube.com/channel/UCAL9BL74HS6Cl3CVX9K18Ng"><i
+                    class="header-social-links fa-brands fa-youtube hover:text-[#FF0000]"></i></a>
+            <a href="https://discord.com/invite/vfy4KDaMXY"><i
+                    class="header-social-links fa-brands fa-discord hover:text-[#7289DA]"></i></a>
             <a href="#"><i class="header-social-links fa-solid fa-envelope hover:text-[#FBBC05]"></i></a>
         </div>
 
