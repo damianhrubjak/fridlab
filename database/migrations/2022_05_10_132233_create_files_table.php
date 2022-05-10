5defa90b7c3b4d56369eb9b807e6235b3317394a
Damián Hrubják <hrubjak.damian@gmail.com>
2022-05-10 13:29:11 +0200

    Created and set up File model

---
+++ database/migrations/2022_05_10_132233_create_files_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->string('source_path', 191);
+            $table->string('file_name', 191);
+            $table->string('size', 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+};
