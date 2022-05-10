20d03493139fd70c2af89e35f66aabf721adc56a
Damián Hrubják <hrubjak.damian@gmail.com>
2022-05-10 13:24:06 +0200

    Created and set up PrintModel

---
+++ app/Http/Controllers/PrintModelController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PrintModel;
+use Illuminate\Http\Request;
+
+class PrintModelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PrintModel  $printModel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PrintModel $printModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PrintModel  $printModel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PrintModel $printModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PrintModel  $printModel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PrintModel $printModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PrintModel  $printModel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PrintModel $printModel)
+    {
+        //
+    }
+}
