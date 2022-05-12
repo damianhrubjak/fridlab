dd529d9a7c5dff7f3d93ec9df21871af14950300
borisbrnkalak <borisbrnkalak@gmail.com>
2022-05-12 21:37:47 +0200

    Added Pixelated icons to Contact page

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -13,24 +13,143 @@
                     corporis perspiciatis.</p>
             </div>
             <div class="grid w-1/2 grid-cols-2 gap-10">
-                <div>
-                    <h3 class="font-heading text-pallette-yellow mb-2 text-lg tracking-wider">Adresa</h3>
-                    <p>Žilina 8512, 012345</p>
+                <div class="flex items-center justify-between gap-3">
+                    <div class="w-[30%] p-4">
+                        <svg class="h-20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 99.81 128.25">
+                            <defs>
+                                <style>
+                                    .cls-1 {
+                                        fill: var(--pallette-yellow);
+                                        stroke: var(--pallette-yellow);
+                                    }
+
+                                </style>
+                            </defs>
+                            <g id="Layer_2" data-name="Layer 2">
+                                <g id="Layer_1-2" data-name="Layer 1">
+                                    <path class="cls-1"
+                                        d="M85.46 57.02h14.22v14.22H85.46zM85.46 42.8h14.22v14.22H85.46zM85.46 28.57h14.22V42.8H85.46z" />
+                                    <path class="cls-1" d="M71.23 71.24h14.23v14.22H71.23z" />
+                                    <path class="cls-1"
+                                        d="M71.23 57.02h14.23v14.22H71.23zM71.23 42.8h14.23v14.22H71.23zM71.23 28.57h14.23V42.8H71.23zM71.23 14.35h14.23v14.22H71.23zM57.02 99.68h14.22v14.22H57.02zM57.02 85.45h14.22v14.23H57.02z" />
+                                    <path class="cls-1" d="M57.02 71.24h14.22v14.22H57.02z" />
+                                    <path class="cls-1"
+                                        d="M57.02 57.02h14.22v14.22H57.02zM57.02 28.57h14.22V42.8H57.02zM57.02 14.35h14.22v14.22H57.02z" />
+                                    <path class="cls-1"
+                                        d="M57.02.13h14.22v14.22H57.02zM42.8 113.91h14.22v14.22H42.8zM42.8 99.68h14.22v14.22H42.8zM42.8 85.45h14.22v14.23H42.8z" />
+                                    <path class="cls-1"
+                                        d="M42.8 71.24h14.22v14.22H42.8zM42.8 14.35h14.22v14.22H42.8z" />
+                                    <path class="cls-1"
+                                        d="M42.8.13h14.22v14.22H42.8zM28.57 99.68h14.22v14.22H28.57zM28.57 85.45h14.22v14.23H28.57z" />
+                                    <path class="cls-1" d="M28.57 71.24h14.22v14.22H28.57z" />
+                                    <path class="cls-1"
+                                        d="M28.57 57.02h14.22v14.22H28.57zM28.57 28.57h14.22V42.8H28.57zM28.57 14.35h14.22v14.22H28.57z" />
+                                    <path class="cls-1"
+                                        d="M28.57.13h14.22v14.22H28.57zM14.35 71.24h14.23v14.22H14.35z" />
+                                    <path class="cls-1"
+                                        d="M14.35 57.02h14.23v14.22H14.35zM14.35 42.8h14.23v14.22H14.35zM14.35 28.57h14.23V42.8H14.35zM14.35 14.35h14.23v14.22H14.35z" />
+                                    <path class="cls-1"
+                                        d="M.13 57.02h14.22v14.22H.13zM.13 42.8h14.22v14.22H.13zM.13 28.57h14.22V42.8H.13z" />
+                                </g>
+                            </g>
+                        </svg>
+                    </div>
+                    <div class="w-[70%]">
+
+                        <h3 class="font-heading text-pallette-yellow mb-2 text-lg tracking-wider">Adresa</h3>
+                        <p>Žilina 8512, 012345</p>
+                    </div>
                 </div>
-                <div>
-                    <h3 class="font-heading text-pallette-yellow mb-2 text-lg tracking-wider">Telefón</h3>
-                    <p><span class="text-pallette-yellow">+</span>421 952 145 369</p>
+
+
+                <div class="flex items-center justify-between gap-3">
+                    <div class="w-[30%] p-4">
+                        <svg class="h-[80px] -rotate-45" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 70.07 128.25">
+                            <defs>
+                                <style>
+                                    .cls-1 {
+                                        fill: var(--pallette-yellow);
+                                        stroke: var(--pallette-yellow);
+                                    }
+
+                                </style>
+                            </defs>
+                            <g id="Layer_2" data-name="Layer 2">
+                                <g id="Layer_1-2" data-name="Layer 1">
+                                    <path class="cls-1"
+                                        d="M23.4 81.58h11.63v11.64H23.4zM23.4 35.03h11.63v11.64H23.4zM58.31 116.49h11.64v11.64H58.31zM58.31 104.85h11.64v11.63H58.31z" />
+                                    <path class="cls-1"
+                                        d="M58.31 93.22h11.64v11.64H58.31zM58.31 23.4h11.64v11.64H58.31zM58.31 11.77h11.64V23.4H58.31z" />
+                                    <path class="cls-1"
+                                        d="M58.31.13h11.64v11.64H58.31zM46.67 116.49H58.3v11.64H46.67zM46.67 104.85H58.3v11.63H46.67z" />
+                                    <path class="cls-1"
+                                        d="M46.67 93.22H58.3v11.64H46.67zM46.67 23.4H58.3v11.64H46.67zM46.67 11.77H58.3V23.4H46.67z" />
+                                    <path class="cls-1"
+                                        d="M46.67.13H58.3v11.64H46.67zM35.03 116.49h11.64v11.64H35.03zM35.03 104.85h11.64v11.63H35.03zM35.03 11.77h11.64V23.4H35.03z" />
+                                    <path class="cls-1"
+                                        d="M35.03.13h11.64v11.64H35.03zM23.4 104.85h11.63v11.63H23.4z" />
+                                    <path class="cls-1"
+                                        d="M23.4 93.22h11.63v11.64H23.4zM23.4 23.4h11.63v11.64H23.4zM23.4 11.77h11.63V23.4H23.4zM11.76 93.22H23.4v11.64H11.76zM11.76 81.58H23.4v11.64H11.76zM11.76 69.94H23.4v11.63H11.76z" />
+                                    <path class="cls-1"
+                                        d="M11.76 58.31H23.4v11.64H11.76zM11.76 46.68H23.4v11.63H11.76zM11.76 35.03H23.4v11.64H11.76z" />
+                                    <path class="cls-1"
+                                        d="M11.76 23.4H23.4v11.64H11.76zM.13 69.94h11.64v11.63H.13z" />
+                                    <path class="cls-1"
+                                        d="M.13 58.31h11.64v11.64H.13zM.13 46.68h11.64v11.63H.13z" />
+                                </g>
+                            </g>
+                        </svg>
+                    </div>
+                    <div class="w-[70%]">
+
+                        <h3 class="font-heading text-pallette-yellow mb-2 text-lg tracking-wider">Telefón</h3>
+                        <p><span class="text-pallette-yellow">+</span>421 952 145 369</p>
+                    </div>
                 </div>
-                <div>
-                    <h3 class="font-heading text-pallette-yellow mb-2 text-lg tracking-wider">Email</h3>
-                    <a href="#">nejakyfajnymail@mail.com</a>
+                <div class="flex items-center justify-between gap-3">
+                    <div class="flex w-[30%] items-center p-4">
+                        <p class="font-heading text-pallette-yellow h-20 text-7xl">@</p>
+                    </div>
+                    <div class="w-[70%]">
+
+                        <h3 class="font-heading text-pallette-yellow mb-2 text-lg tracking-wider">Email</h3>
+                        <a href="#">nejakyfajnymail@mail.com</a>
+                    </div>
                 </div>
-                <div>
-                    <h3 class="font-heading text-pallette-yellow mb-2 text-lg tracking-wider">Dostupnosť</h3>
-                    <p>
-                        Pondelok - Sobota <br>
-                        8:00 - 18:00
-                    </p>
+                <div class="flex items-center justify-between gap-3">
+                    <div class="w-[30%] p-4">
+                        <svg class="h-16" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0"
+                            y="0" viewBox="0 0 128 128" style="enable-background:new 0 0 128 128" xml:space="preserve">
+                            <style>
+                                .st0 {
+                                    fill: var(--pallette-yellow);
+                                    stroke: var(--pallette-yellow);
+                                }
+
+                            </style>
+                            <g id="XMLID_00000021811822576277620990000011484516247787024559_">
+                                <path class="st0"
+                                    d="M116.36 81.45H128v11.64h-11.64zM116.36 69.82H128v11.63h-11.64zM116.36 58.18H128v11.64h-11.64zM116.36 46.55H128v11.63h-11.64zM116.36 34.91H128v11.64h-11.64zM116.36 23.27H128v11.64h-11.64zM104.73 104.73h11.63v11.63h-11.63zM104.73 93.09h11.63v11.64h-11.63zM104.73 81.45h11.63v11.64h-11.63zM104.73 69.82h11.63v11.63h-11.63zM104.73 46.55h11.63v11.63h-11.63zM104.73 34.91h11.63v11.64h-11.63zM104.73 23.27h11.63v11.64h-11.63z" />
+                                <path class="st0"
+                                    d="M104.73 11.64h11.63v11.63h-11.63zM93.09 104.73h11.64v11.63H93.09zM93.09 93.09h11.64v11.64H93.09zM93.09 23.27h11.64v11.64H93.09z" />
+                                <path class="st0"
+                                    d="M93.09 11.64h11.64v11.63H93.09zM81.45 116.36h11.64V128H81.45zM81.45 104.73h11.64v11.63H81.45zM81.45 58.18h11.64v11.64H81.45zM81.45 11.64h11.64v11.63H81.45zM81.45 0h11.64v11.64H81.45zM69.82 116.36h11.63V128H69.82zM69.82 104.73h11.63v11.63H69.82zM69.82 58.18h11.63v11.64H69.82zM69.82 11.64h11.63v11.63H69.82zM69.82 0h11.63v11.64H69.82zM58.18 116.36h11.64V128H58.18zM58.18 58.18h11.64v11.64H58.18zM58.18 46.55h11.64v11.63H58.18zM58.18 34.91h11.64v11.64H58.18zM58.18 0h11.64v11.64H58.18zM46.55 116.36h11.63V128H46.55zM46.55 104.73h11.63v11.63H46.55zM46.55 11.64h11.63v11.63H46.55zM46.55 0h11.63v11.64H46.55zM34.91 116.36h11.64V128H34.91zM34.91 104.73h11.64v11.63H34.91zM34.91 11.64h11.64v11.63H34.91zM34.91 0h11.64v11.64H34.91zM23.27 104.73h11.64v11.63H23.27zM23.27 93.09h11.64v11.64H23.27zM23.27 23.27h11.64v11.64H23.27z" />
+                                <path class="st0"
+                                    d="M23.27 11.64h11.64v11.63H23.27zM11.64 104.73h11.63v11.63H11.64zM11.64 93.09h11.63v11.64H11.64zM11.64 81.45h11.63v11.64H11.64zM11.64 69.82h11.63v11.63H11.64zM11.64 46.55h11.63v11.63H11.64zM11.64 34.91h11.63v11.64H11.64z" />
+                                <path class="st0" d="M11.64 23.27h11.63v11.64H11.64z" />
+                                <path class="st0"
+                                    d="M11.64 11.64h11.63v11.63H11.64zM0 81.45h11.64v11.64H0zM0 69.82h11.64v11.63H0zM0 58.18h11.64v11.64H0zM0 46.55h11.64v11.63H0zM0 34.91h11.64v11.64H0z" />
+                            </g>
+                        </svg>
+                    </div>
+                    <div class="w-[70%]">
+
+                        <h3 class="font-heading text-pallette-yellow mb-2 text-lg tracking-wider">Dostupnosť</h3>
+                        <p>
+                            Pondelok - Sobota <br>
+                            8:00 - 18:00
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
