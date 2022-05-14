64ac0dfdbc576d987214ea2b903782070344d7c7
borisbrnkalak <borisbrnkalak@gmail.com>
2022-05-14 11:51:20 +0200

    Admin -> active blog - bug fixed

--- resources/views/admin/items/side-blog-item.blade.php
+++ resources/views/admin/items/side-blog-item.blade.php
@@ -1,10 +1,7 @@
-@php
-dd(url()->current());
-@endphp
-
-<a class="blog-itm {{ strtolower(str_replace(' ', '-', $b->title)) === $b->slug ? 'active' : '' }} mb-5 flex w-full items-center justify-between"
+<a class="blog-itm {{ strpos(url()->current(), $b->slug) ? 'active' : '' }} mb-5 flex w-full items-center justify-between"
     href="{{ route('admin-pages.blogy.show', $b->slug) }}">
     <div class="w-[30%]"><img class="w-full" src="{{ route('file-thumbnail-serve', $b->image->slug) }}"
             alt=""></div>
-    <span class="w-[70%] px-2 text-center font-bold text-white">{{ $b->title }}</span>
+    <span
+        class="{{ strpos(url()->current(), $b->slug) ? 'active-text' : '' }} w-[70%] px-2 text-center font-bold text-white">{{ $b->title }}</span>
 </a>
