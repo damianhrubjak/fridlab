2a0ae1268f68f6d1f14f12cbbe8560ef5b1d0c03
borisbrnkalak <borisbrnkalak@gmail.com>
2022-05-15 13:03:48 +0200

    Fixed bug with overflowing text

--- resources/views/pages/show-blog.blade.php
+++ resources/views/pages/show-blog.blade.php
@@ -19,7 +19,7 @@
                             <h2 class="text-5xl font-bold text-white">{{ $blog->title }}</h2>
                         </div>
 
-                        <div class="prose ml-auto mr-auto max-w-[1200px] text-white">
+                        <div class="prose ml-auto mr-auto max-w-[1200px] break-all text-white">
                             {!! html_entity_decode($blog->text) !!}
                         </div>
                     </div>
