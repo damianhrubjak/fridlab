ad7301eac94dda615029e0bedd925b45cdb19578
Damián Hrubják <hrubjak.damian@gmail.com>
2022-05-16 20:49:41 +0200

    Show PrintModel added link to FileManager

--- resources/views/admin/inc/side-print-models.blade.php
+++ resources/views/admin/inc/side-print-models.blade.php
@@ -2,22 +2,28 @@
     <div class="w-full overflow-y-scroll">
         <h2 class="bg-slate-900 px-4 py-8 text-2xl font-bold text-white">3D modely</h2>
         <div class="">
-            @foreach ($printModels as $menuPrintModel) @include(
-                'admin.items.side-print-model-item',
-                compact(['menuPrintModel', 'printModel'])
-            )
-                @endforeach
+            @foreach ($printModels as $menuPrintModel)
+                @include(
+                    'admin.items.side-print-model-item',
+                    compact(['menuPrintModel', 'printModel'])
+                )
+            @endforeach
         </div>
     </div>
     <div class="flex justify-center gap-8 bg-slate-900 px-2 py-4">
-        <a href="#" class="icon-link w-1/2 text-center text-white transition duration-300">
-            <i class="fa-solid fa-trash text-3xl"></i>
-            <p class="mt-2">Odstrániť</p>
-        </a>
         <a href="{{ route('admin-pages.modely.edit', $printModel->slug) }}"
             class="icon-link w-1/2 text-center text-white transition duration-300">
             <i class="fa-solid fa-pen-to-square text-3xl"></i>
             <p class="mt-2">Upraviť</p>
+        </a>
+        <a href="{{ route('admin-pages.print-model-files.index', $printModel->slug) }}"
+            class="icon-link w-1/2 text-center text-white transition duration-300">
+            <i class="fa-solid fa-folder text-3xl"></i>
+            <p class="mt-2">Súbory</p>
+        </a>
+        <a href="#" class="icon-link w-1/2 text-center text-white transition duration-300">
+            <i class="fa-solid fa-trash text-3xl"></i>
+            <p class="mt-2">Odstrániť</p>
         </a>
     </div>
 </aside>
