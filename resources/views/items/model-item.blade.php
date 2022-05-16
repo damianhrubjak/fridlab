6ac4a726d395415ce1b58b26488cb0632b17d092
Damián Hrubják <hrubjak.damian@gmail.com>
2022-05-16 21:49:47 +0200

    FE -> printModel Fixed visual bugs

--- resources/views/items/model-item.blade.php
+++ resources/views/items/model-item.blade.php
@@ -6,11 +6,10 @@
         <img src="{{ route('file-thumbnail-serve', $mainImage->slug) }}" alt="IMAGE" class="w-full object-cover">
     </div>
     <div class="p-3">
-
         <h2 class="mb-2 text-3xl">{{ $model->title }}</h2>
         <p>{{ addThreeDots(strip_tags(html_entity_decode($model->text)), 200) }}</p>
-        <div class="w-full">
-            <a class="bg-pallette-yellow text-pallette-black ml-auto mr-0 block w-max py-2 px-4 font-bold transition duration-150 hover:bg-[#BAAD25]"
+        <div class="mt-8 w-full">
+            <a class="bg-pallette-yellow text-pallette-black font-heading ml-auto mr-0 block w-max py-2 px-4 font-bold transition duration-150 hover:bg-[#BAAD25]"
                 href="{{ route('fe-pages.modelShowPage', $model->slug) }}">Otvoriť</a>
         </div>
     </div>
--- resources/views/pages/show-model.blade.php
+++ resources/views/pages/show-model.blade.php
@@ -20,14 +20,14 @@
                                     alt="{{ $printModel->title }}">
                             </div>
                             <div class="w-1/2">
-                                <div class="flex w-full gap-4">
+                                <div class="grid w-full grid-cols-3 gap-4">
                                     @foreach ($fileGroups['image'] as $image)
-                                        <img class="w-1/3"
+                                        <img class="h-full w-full"
                                             src="{{ route('file-thumbnail-serve', $image->slug) }}"
                                             alt="{{ $printModel->title }}">
                                     @endforeach
                                 </div>
-                                <div class="mt-4 flex w-full gap-4">
+                                <div class="mt-4 flex w-full flex-wrap gap-4">
                                     @foreach ($fileGroups['file'] as $file)
                                         <a href="{{ route('file-download', $file->slug) }}" download
                                             class="border-pallette-yellow hover:bg-pallette-yellow hover:text-pallette-black flex w-max items-center border-2 py-2 px-4 font-bold text-white transition duration-300">
