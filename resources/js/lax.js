554f34e6bd62a594171f3400eb7c80077e35b07f
Damián Hrubják <hrubjak.damian@gmail.com>
2022-05-16 14:31:04 +0200

    Polished printer scroll animation

--- resources/js/lax.js
+++ resources/js/lax.js
@@ -1,6 +1,9 @@
 import lax from "lax.js";
 
 const moveVal = 100;
+let fraction = 0,
+    multiplier = 0,
+    newValue = 0;
 
 window.addEventListener("DOMContentLoaded", () => {
     lax.init();
@@ -24,25 +27,29 @@ window.addEventListener("DOMContentLoaded", () => {
         scrollY: {
             translateX: [
                 ["index", 300],
-                [0, moveVal * 6], //70 * 8
+                [0, moveVal * 6],
                 {
-                    // calculate value, to make head move forth and back
-                    // if val is from 0 to 80 -> head is moving to right
-                    // if val is from 80 to 0 -> head is moving to left
+                    //? calculate value, to make head move forth and back
+                    //* if val is from 0 to 80 -> head is moving to right
+                    //* if val is from 80 to 0 -> head is moving to left
                     cssFn: (val) => {
-                        const fraction = val / moveVal;
+                        fraction = val / moveVal;
 
-                        // multiplier how many times is scrolled position greater than moveVal
-                        const multiplier = Math.round(fraction);
+                        //* multiplier how many times is scrolled position greater than moveVal
+                        multiplier = Math.floor(fraction);
+
+                        // 0 + 80 * (3.8 - 3) => 80 * 0.8 => 10px
+                        newValue = moveVal * (fraction - multiplier);
 
                         // if is multiplier is even head is moving to right
                         // if is multiplier is odd head is moving to left
                         if (multiplier % 2 === 0) {
                             // formula: 0 + 80 * (3.8 - 3) => 80 * 0.8 => 10px
-                            return moveVal * (fraction - multiplier);
+                            return newValue < 0 ? 0 : newValue;
                         } else {
+                            //* when head is going left, we have to subtract moveVal from newValue
                             // formula: 80 - ( 80 * (3.8 - 3)) => 80 - (80 * 0.8) => 70px
-                            return 70 - moveVal * (fraction - multiplier);
+                            return newValue < 0 ? 0 : moveVal - newValue;
                         }
                     },
                 },
