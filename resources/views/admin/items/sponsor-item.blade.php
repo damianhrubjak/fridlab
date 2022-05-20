a392e43d766c9db24337dce51316a9131b57cec3
Damián Hrubják <hrubjak.damian@gmail.com>
2022-05-20 09:26:38 +0200

    Sponsor item -> changed image url, small UI tweaks

--- resources/views/admin/items/sponsor-item.blade.php
+++ resources/views/admin/items/sponsor-item.blade.php
@@ -1,11 +1,10 @@
-<div class="bg-pallette-black flex flex-col text-white">
-    <div class="h-full w-full">
-        <img class="h-full w-full object-cover" src="{{ route('file-thumbnail-serve', $sponsor->image->slug) }}"
+<div class="text-pallette-black flex flex-col bg-slate-100">
+    <div class="h-full w-full p-4">
+        <img class="h-28 w-full object-contain" src="{{ route('file-serve', $sponsor->image->slug) }}"
             alt="{{ $sponsor->image->file_name }}">
     </div>
 
     <div class="h-full w-full py-5 px-6">
-
         <div class="flex justify-between">
             <h2 class="mb-2 text-3xl font-bold">{{ $sponsor->name }}</h2>
             <form action="{{ route('admin-pages.sponzori.destroy', $sponsor->id) }}" method="POST">
